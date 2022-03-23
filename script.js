fc84e26ed51ad8b65597626103045f15be8445d0
CatherineHonors <cathonors@gmail.com>
2022-03-23 15:53:27 -0400

    added table dynamically to what's next page, intend to move to character stats

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ h2.textContent = "Yes, character stats as in a video game...";
 
 document.querySelector("body").appendChild(h2);
 
-let tlou = document.getElementsByClassName("tlou");
-
-tlou.forEach(tlou => {
-    tlou.addEventListener('click', function handleClick(event) {
-        console.log('tlou clicked', event);
-
-        tlou.setAttribute()
-    })
+let chartable = '';
+let rows = 4;
+let cols = 2;
+for (let r = 0; r < rows;r++){
+    table += '<tr>';
+        for(let c = 0; c < cols;c++) {
+            table += '<td>' + c + '</td>';
+        }
+    table += '</tr>';
 }
