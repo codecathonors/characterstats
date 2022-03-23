67feefc8c9b2641a7c72a93d5d516be733f70411
CatherineHonors <cathonors@gmail.com>
2022-03-23 14:52:12 -0400

    formatting about me portion of charstats.html

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ h2.textContent = "Yes, character stats as in a video game...";
 
 document.querySelector("body").appendChild(h2);
 
+let tlou = document.getElementsByClassName("tlou");
+
+tlou.forEach(tlou => {
+    tlou.addEventListener('click', function handleClick(event) {
+        console.log('tlou clicked', event);
+
+        tlou.setAttribute()
+    })
+}
